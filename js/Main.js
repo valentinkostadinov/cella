bbf3c9a113bfea6ef2155c2d7dbcccbd9673d4f5
unknown <Valentin@.(none)>
2011-09-04 14:32:31 +0300

    - fixed key input

--- js/Main.js
+++ js/Main.js
@@ -8,34 +8,15 @@ function onReady() {
 
 	Grid.init(document.getElementById("grid"));
 
-	$("#helpdialog").dialog({
-		autoOpen: false,
-		modal: true,
-		draggable: false,
-		resizable: false,
-		width: "400",
-	});
-	$("#help").button({
-		text: false,
-		label: "Help [H]",
-		icons: {
-			primary: "ui-icon-info"
-		}
-	})
-	.click( function() {
-		$("#helpdialog").dialog("open");
-	});
-
 	$("#clear").button({
-		text: false,
-		label: "Clear Grid [C]",
-		icons: {
-			primary: "ui-icon-document"
+		text : false,
+		label : "Clear Grid [C]",
+		icons : {
+			primary : "ui-icon-document"
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		AutomatonManager.halt();
-		if (!AutomatonManager.pattern) {
+		if(!AutomatonManager.pattern) {
 			AutomatonManager.pattern = Automaton.toPattern();
 		}
 		Automaton.clear();
@@ -44,62 +25,56 @@ function onReady() {
 	});
 
 	$("#reset").button({
-		text: false,
-		label: "Reset To Last Edit [R]",
-		icons: {
-			primary: "ui-icon-arrowreturnthick-1-w",
+		text : false,
+		label : "Reset To Last Edit [R]",
+		icons : {
+			primary : "ui-icon-arrowreturnthick-1-w",
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		AutomatonManager.reset();
 		Grid.fitPattern();
 		Grid.paint();
 	});
-
 	var playOptions = {
-		label: "Play [Space]",
-		icons: {
-			primary: "ui-icon-play",
+		label : "Play [Space]",
+		icons : {
+			primary : "ui-icon-play",
 		},
 	};
 	var pauseOptions = {
-		label: "Pause [Space]",
-		icons: {
-			primary: "ui-icon-pause",
+		label : "Pause [Space]",
+		icons : {
+			primary : "ui-icon-pause",
 		},
 	};
 	$("#play").button({
-		text: false,
-	})
-	.click( function() {
+		text : false,
+	}).click(function() {
 		AutomatonManager.toggle();
 	});
 
 	$("#play").button("option", playOptions);
 
 	$("#nextgen").button({
-		text: false,
-		label: "Next Generation [G]",
-		icons: {
-			primary: "ui-icon-arrow-1-e",
+		text : false,
+		label : "Next Generation [G]",
+		icons : {
+			primary : "ui-icon-arrow-1-e",
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		AutomatonManager.step(1);
 	});
-
 	var getNextStepLabel = function() {
 		return "Step Over " + AutomatonManager.getSteps() + " Generation(s) [S]";
 	}
 
 	$("#nextstep").button({
-		text: false,
-		label: getNextStepLabel(),
-		icons: {
-			primary: "ui-icon-arrowthick-1-e",
+		text : false,
+		label : getNextStepLabel(),
+		icons : {
+			primary : "ui-icon-arrowthick-1-e",
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		AutomatonManager.step();
 	});
 
@@ -107,37 +82,34 @@ function onReady() {
 
 	$("#gridlines")[0].checked = Grid.visibleGridLines;
 	$("#gridlines").button({
-		text: false,
-		label: "Toggle Grid Lines [L]",
-		icons: {
-			primary: "ui-icon-calculator"
+		text : false,
+		label : "Toggle Grid Lines [L]",
+		icons : {
+			primary : "ui-icon-calculator"
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		Grid.toggleGridLines();
 	});
 
 	$("#fitpattern").button({
-		text: false,
-		label: "Fit Pattern To Window [F]",
-		icons: {
-			primary: "ui-icon-arrow-4-diag"
+		text : false,
+		label : "Fit Pattern To Window [F]",
+		icons : {
+			primary : "ui-icon-arrow-4-diag"
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		Grid.fitPattern(true);
 		Grid.paint();
 	});
 
 	$("#autofit")[0].checked = Grid.autoFit;
 	$("#autofit").button({
-		text: false,
-		label: "Toggle Auto-Fit [A]",
-		icons: {
-			primary: "ui-icon-check"
+		text : false,
+		label : "Toggle Auto-Fit [A]",
+		icons : {
+			primary : "ui-icon-check"
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		Grid.autoFit = !Grid.autoFit;
 	});
 
@@ -145,125 +117,115 @@ function onReady() {
 
 	$("#scale")[0].title = "Scale [click to reset]";
 	$("#scale").button({
-		label: Grid.getScaleString(),
-	})
-	.click( function() {
+		label : Grid.getScaleString(),
+	}).click(function() {
 		Grid.setZoom(Grid.getZoom(Grid.DEFAULT_SCALE), true);
 	});
 
 	$("#zoomin").parent().buttonset();
 	$("#zoomslider").slider({
-		min: 0,
-		max: Grid.getMaxZoom(),
-		value: Grid.getZoom(),
-		slide: function(event, ui) {
+		min : 0,
+		max : Grid.getMaxZoom(),
+		value : Grid.getZoom(),
+		slide : function(event, ui) {
 			Grid.setZoom(ui.value);
 		},
-
 	});
 	$("#zoomin").button({
-		text: false,
-		label: "Zoom In [Ctrl-Up]",
-		icons: {
-			primary: "ui-icon-zoomin"
+		text : false,
+		label : "Zoom In [Ctrl-Up]",
+		icons : {
+			primary : "ui-icon-zoomin"
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		Grid.zoom(1);
 	});
 
 	$("#zoomout").button({
-		text: false,
-		label: "Zoom Out [Ctrl-Down]",
-		icons: {
-			primary: "ui-icon-zoomout"
+		text : false,
+		label : "Zoom Out [Ctrl-Down]",
+		icons : {
+			primary : "ui-icon-zoomout"
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		Grid.zoom(-1);
 	});
-
 	// speed
 	$("#speed")[0].title = "Speed [click to reset]";
 	$("#speed").parent().buttonset();
 	$("#speed").button({
-		label: AutomatonManager.getSpeedString(),
-	})
-	.click( function() {
+		label : AutomatonManager.getSpeedString(),
+	}).click(function() {
 		AutomatonManager.setSpeed(AutomatonManager.DEFAULT_SPEED);
 	});
 
 	$("#slower").button({
-		text: false,
-		label: "Slower [Ctrl-Left]",
-		icons: {
-			primary: "ui-icon-minus"
+		text : false,
+		label : "Slower [Ctrl-Left]",
+		icons : {
+			primary : "ui-icon-minus"
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		AutomatonManager.speedDown();
 	});
 
 	$("#faster").button({
-		text: false,
-		label: "Faster [Ctrl-Right]",
-		icons: {
-			primary: "ui-icon-plus",
+		text : false,
+		label : "Faster [Ctrl-Right]",
+		icons : {
+			primary : "ui-icon-plus",
 		},
-	})
-	.click( function() {
+	}).click(function() {
 		AutomatonManager.speedUp();
 	})
 
 	$("#speedslider").slider({
-		min: AutomatonManager.MIN_SPEED,
-		max: AutomatonManager.MAX_SPEED,
-		value: AutomatonManager.speed,
-		slide: function(event, ui) {
+		min : AutomatonManager.MIN_SPEED,
+		max : AutomatonManager.MAX_SPEED,
+		value : AutomatonManager.speed,
+		slide : function(event, ui) {
 			AutomatonManager.setSpeed(ui.value);
 		}
-
-	});
-
-	Rules.list.forEach( function(r, index) {
-		$("#rulelist").append(
-		"<li><a href='#' " +
-		"rule-index='" + index + "' " +
-		">" + r.toString() + "</a></li>"
-		);
 	});
 
 	$("#rule")[0].title = "Automaton Rule (click to edit)";
 	$("#rule").button({
-		label: Automaton.rule.toString(),
+		label : Automaton.rule.toString(),
+	}).click(function() {
+		$("#ruledialog").dialog("open");
 	});
+
 	$("#ruleselect").button({
-		text: false,
-		label: "Select A Rule",
-		icons: {
-			primary: "ui-icon-triangle-1-s",
+		text : false,
+		label : "Select A Rule",
+		icons : {
+			primary : "ui-icon-triangle-1-s",
 		},
 	})
+
+	// populate list
+	Rules.list.forEach(function(r, index) {
+		$("#rulelist").append("<li><a href='#' " + "rule-index='" + index + "' " + ">" + r.toString() + "</a></li>");
+	});
+
 	$("#rulelist").menu({
-		select: function(e, ui) {
+		select : function(e, ui) {
 			$("#rule").button("option", "label", ui.item.text());
 			var ruleIndex = ui.item.children(0).attr("rule-index");
 			Automaton.setRule(Rules.list[ruleIndex]);
 			document.activeElement.blur();
 		},
-
-	})
-	.popup().parent().buttonset({
-		items: "button",
+	}).popup().parent().buttonset({
+		items : "button",
 	});
 
+	// stats
 	var population = $("#population")[0];
 	var generation = $("#generation")[0];
 	var updateStats = function() {
 		population.innerHTML = Automaton.size;
 		generation.innerHTML = Automaton.generation;
 	};
-
 	updateStats();
 
 	// event binding
@@ -289,112 +251,81 @@ function onReady() {
 
 	AutomatonManager.bind('paused', function() {
 		$("#play").button("option", AutomatonManager.paused ? playOptions : pauseOptions);
-		if (AutomatonManager.paused) {
+		if(AutomatonManager.paused) {
 			latency.innerHTML = "";
 		}
 	});
-
 	var latency = $("#latency")[0];
 	AutomatonManager.bind('tick', function() {
 		latency.innerHTML = new Number(AutomatonManager.getMovingAverage()).toFixed(1) + "ms";
 	});
 
-	// keyboard setup
+	$(document).bind('keydown', 'space', function() {
+		$("#play").click();
+	});
+	$(document).bind('keydown', 'r', function() {
+		$("#reset").click();
+	});
+	$(document).bind('keydown', 'c', function() {
+		$("#clear").click();
+	});
+	$(document).bind('keydown', 's', function() {
+		$("#nextstep").click();
+	});
+	$(document).bind('keydown', 'g', function() {
+		$("#nextgen").click();
+	});
+	$(document).bind('keydown', 'l', function() {
+		$("#gridlines").click();
+	});
+	$(document).bind('keydown', 'a', function() {
+		$("#autofit").click();
+	});
+	$(document).bind('keydown', 'f', function() {
+		$("#fitpattern").click();
+	});
+
+	$(document).bind('keydown', 'ctrl+left', function() {
+		AutomatonManager.speedDown();
+	});
+	$(document).bind('keydown', 'ctrl+right', function() {
+		AutomatonManager.speedUp();
+	});
+	$(document).bind('keydown', 'ctrl+up', function() {
+		Grid.zoom(1);
+	});
+	$(document).bind('keydown', 'ctrl+down', function() {
+		Grid.zoom(-1);
+	});
+	var pan;
 	(function() {
-		var KEY = {
-			CTRL: 17,
-			SPACE: 32,
-			LEFT: 37,
-			UP: 38,
-			RIGHT: 39,
-			DOWN: 40,
-			A: 65,
-			C: 67,
-			F: 70,
-			G: 71,
-			H: 72,
-			L: 76,
-			R: 82,
-			S: 83,
-		};
-
-		var velocity = (Grid.scale > 0 ? Grid.scale : 1);
 		var accelaration = 1.5;
-
-		$(document).keydown( function(event) {
+		var velocity = (Grid.scale > 0 ? Grid.scale : 1);
+		pan = function(x, y) {
 			var delta = Math.floor(velocity += accelaration);
-			if (!event.ctrlKey) {
-				switch (event.which) {
-					case KEY.H:
-						$("#help").click();
-						break;
-					case KEY.C:
-						$("#clear").click();
-						break;
-					case KEY.SPACE:
-						$("#play").click();
-						break;
-					case KEY.S:
-						$("#nextstep").click();
-						break;
-					case KEY.G:
-						$("#nextgen").click();
-						break;
-					case KEY.L:
-						$("#gridlines").click();
-						break;
-					case KEY.A:
-						$("#autofit").click();
-						break;
-					case KEY.F:
-						$("#fitpattern").click();
-						break;
-					case KEY.R:
-						$("#reset").click();
-						break;
-					case KEY.LEFT:
-						Grid.pan(delta, 0);
-						break;
-					case KEY.RIGHT:
-						Grid.pan(-delta, 0);
-						break;
-					case KEY.UP:
-						Grid.pan(0, delta);
-						break;
-					case KEY.DOWN:
-						Grid.pan(0, -delta);
-						break;
-				}
-			} else {
-				// ctrl pressed
-				switch (event.which) {
-					case KEY.LEFT:
-						AutomatonManager.speedDown();
-						break;
-					case KEY.RIGHT:
-						AutomatonManager.speedUp();
-						break;
-					case KEY.UP:
-						Grid.zoom(1)
-						break;
-					case KEY.DOWN:
-						Grid.zoom(-1);
-						break;
-				}
-			};
-		});
-
-		$(document).keyup( function() {
+			Grid.pan(delta * x, delta * y);
+		};
+		$(document).bind('keyup', 'up down right left', function() {
 			velocity = Grid.scale > 0 ? Grid.scale : 1;
 		});
-
 	})();
 
-	// REMOVE ME
+	$(document).bind('keydown', 'up', function() {
+		pan(0, 1);
+	});
+	$(document).bind('keydown', 'down', function() {
+		pan(0, -1);
+	});
+	$(document).bind('keydown', 'right', function() {
+		pan(-1, 0);
+	});
+	$(document).bind('keydown', 'left', function() {
+		pan(1, 0);
+	});
+
 	$.get("patterns/AQUA25.LIF", function(data) {
 		Automaton.addPattern(Patterns.loadLIF(data).positions);
 		Grid.fitPattern();
 		Grid.paint();
 	});
-
 }
